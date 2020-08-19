15f0f8979ad6184d2346a4ec42512ed1014a5bfc
komaki1996 <komaki1996@yahoo.co.jp>
2020-08-19 13:10:09 +0900

    Update SmartMirrorSetUp.sh

--- SmartMirrorSetUp.sh
+++ SmartMirrorSetUp.sh
@@ -2,8 +2,8 @@
 bash -c "$(curl -sL https://raw.githubusercontent.com/sdetweil/MagicMirror_scripts/master/raspberry.sh)"
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt-get install -y nodejs
-git clone https://github.com/MichMich/MagicMirror /home/pi/
-cp /home/pi/MagicMirror/config.js.sample /home/pi/MagicMirror/config.js
-chown -R pi:pi /home/pi/MagicMirror
-npm install --prefix own -R pi:pi own -R pi:pi /home/pi/MagicMirror
-npm start --prefix /home/pi/MagicMirror/
+git clone https://github.com/MichMich/MagicMirror $HOME/MagicMirror
+cp $HOME/MagicMirror/config.js.sample $HOME/MagicMirror/config.js
+chown -R $USER:$USER $HOME/MagicMirror
+npm install --prefix own -R $USER:$USER own -R $USER:$USER $HOME/MagicMirror
+npm start --prefix $HOME/MagicMirror/
